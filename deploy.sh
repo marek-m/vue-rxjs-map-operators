0fa9bffd80d3eaf2540482601c7611d859474f55
marek-m <marek.matysk@gmail.com>
2018-11-20 23:27:05 +0100

    deploy

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:marek-m/vue-rxjs-map-operators.git master:gh-pages
+
+cd -
